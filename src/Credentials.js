2e499faec7bd72607b3d386117c6623739bdeaf3
jvetting <jvetting@iastate.edu>
2021-06-18 14:46:18 -0500

    Only public scope. Working on user auth

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'cd855b6e22c9496b85706c09c2ce2ed3',
+        ClientSecret: '00da0d0878cc4acfb60ab169c4ae0e92'
+    }
+}
+
+export { Credentials };
