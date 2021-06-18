91accd6306890761d16d44b933561c899bf17f81
Jonathan Vetting <53095470+jvetting@users.noreply.github.com>
2021-06-18 15:08:17 -0500

    Delete Credentials.js
    Deleting since wasn't included in gitignore

--- src/Credentials.js
+++
@@ -1,9 +0,0 @@
-const Credentials = () => {
-
-    return {
-        ClientId: 'cd855b6e22c9496b85706c09c2ce2ed3',
-        ClientSecret: '00da0d0878cc4acfb60ab169c4ae0e92'
-    }
-}
-
-export { Credentials };
